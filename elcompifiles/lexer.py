8864a228b08816cd601469dcf1030638c58ab07c
Sebastián Zapata <63472538+sebastianzapatar@users.noreply.github.com>
2023-07-28 11:14:51 -0500

    Se crea para leer variables, crear variables y números

--- elcompifiles/lexer.py
+++ elcompifiles/lexer.py
@@ -25,18 +25,34 @@ class Lexer:
             literal=self._read_identifier()
             token_type=lookup_token_type(literal)
             return Token(token_type,literal)
+        elif self._is_number(self._character):
+            literal=self._read_number()
+            token_type=lookup_token_type(literal)
+            return Token(TokenType.INTEGER,literal)
         else:
             print(self._character)
             token=Token(TokenType.ILLEGAL,self._character)
         self._read_character()
         return token
+    
     def _is_letter(self,character:str)->bool:
        return bool(match(r'^[a-záéíóúA-ZÁÉÍÓÚñÑ_]$', character))
+    
+    def _is_number(self,character:str)->bool:
+        return bool(match(r'^\d$', character))
+    
+    def _read_number(self)->str:
+        initial_position=self._position
+        while self._is_number(self._character):
+            self._read_character()
+        return self._source[initial_position:self._position]
+    
     def _read_identifier(self)->str:
         initial_position=self._position
         while self._is_letter(self._character):
             self._read_character()
         return self._source[initial_position:self._position]
+    
     def _read_character(self)->None:
         if self._read_position>=len(self._source):
             self._character=''
@@ -44,6 +60,7 @@ class Lexer:
             self._character=self._source[self._read_position]
         self._position=self._read_position
         self._read_position+=1
+        
     def _skip_whitespace(self) -> None:
         while match(r'^\s$', self._character):
             self._read_character()

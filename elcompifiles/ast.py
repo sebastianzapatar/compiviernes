019d206892e921e937ce6dad98788b1406582e50
Sebastián Zapata <63472538+sebastianzapatar@users.noreply.github.com>
2023-08-25 08:14:27 -0500

    Se organiza el código para que pase las pruebas

--- elcompifiles/ast.py
+++ elcompifiles/ast.py
@@ -1,40 +1,82 @@
 from abc import(
     ABC,
-    abstractclassmethod
+    abstractmethod
 )
-from typing import List
+from typing import (
+    List,
+    Optional
+)
 
 from elcompifiles.tokens import Token
 class ASTNode(ABC):
-    @abstractclassmethod
-    def token_literal(self)->str:
+
+    @abstractmethod
+    def token_literal(self) -> str:
         pass
-    @abstractclassmethod
+
+    @abstractmethod
     def __str__(self) -> str:
         pass
+
 
 class Statement(ASTNode):
-    def __init__(self,token:Token) -> None:
-        self.token=token
-    def token_literal(self)->str:
+
+    def __init__(self, token: Token) -> None:
+        self.token = token
+
+    def token_literal(self) -> str:
         return self.token.literal
+
 
 class Expression(ASTNode):
-    def __init__(self,token:Token) -> None:
-        self.token=token
-    def token_literal(self)->str:
+
+    def __init__(self, token: Token) -> None:
+        self.token = token
+
+    def token_literal(self) -> str:
         return self.token.literal
 
+
 class Program(ASTNode):
-    def __init__(self,statements:List[Statement]) -> None:
-        self.statements=statements
-    
-    def token_literal(self)->str:
-        if len(self.statements)>0:
+
+    def __init__(self, statements: List[Statement]) -> None:
+        self.statements = statements
+
+    def token_literal(self) -> str:
+        if len(self.statements) > 0:
             return self.statements[0].token_literal()
+
         return ''
+
     def __str__(self) -> str:
-        out:List[str]=[]
+        out: List[str] = []
         for statement in self.statements:
             out.append(str(statement))
+
         return ''.join(out)
+
+
+class Identifier(Expression):
+
+    def __init__(self,
+                 token: Token,
+                 value: str) -> None:
+        super().__init__(token)
+        self.value = value
+
+    def __str__(self) -> str:
+        return self.value
+
+
+class LetStatement(Statement):
+
+    def __init__(self,
+                 token: Token,
+                 name: Optional[Identifier] = None,
+                 value: Optional[Expression] = None) -> None:
+        super().__init__(token)
+        self.name = name
+        self.value = value
+
+    def __str__(self) -> str:
+        return f'{self.token_literal()} {str(self.name)} = {str(self.value)};'
